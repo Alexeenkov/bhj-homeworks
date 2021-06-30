bd74563eaa1428964bc77a2f4eff60c7f468d1cc
Ilya Alekseenkov <rmfighht@yandex.ru>
2021-06-30 16:08:11 +0300

    Доработал задание 3.2

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -2,14 +2,23 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     let signIn = document.getElementById('signin');
-    let button = document.getElementById('signin__btn');
+    let signInBtn = document.getElementById('signin__btn');
     let form = document.getElementById('signin__form');
     let welcome = document.getElementById('welcome');
     let userId = document.getElementById('user_id');
+    let signOutBtn = document.getElementById('signout__btn');
 
-    signIn.classList.add('signin_active');
+    // Проверяем, есть ли id у пользователя:
+    if (localStorage.user_id === undefined) {
+        signIn.classList.add('signin_active');
+    } else {
+        userId.textContent = localStorage.user_id;
+        welcome.classList.add('welcome_active');
+        signOutBtn.classList.add('welcome_active');
+    }
+
     // При клике на кнопку входа:
-    button.addEventListener('click', (e) => {
+    signInBtn.addEventListener('click', (e) => {
         e.preventDefault();
         // Собираем данные из формы, создаем и отправляем запрос с данными формы:
         let formData = new FormData(form);
@@ -27,10 +36,20 @@ document.addEventListener('DOMContentLoaded', () => {
                     welcome.classList.add('welcome_active'); // Показываем приветствие
                     userId.textContent = response.user_id; // Указываем идентификатор пользователя
                     localStorage.user_id = response.user_id; // Записываем его в локальное хранилище
+                    signOutBtn.classList.add('welcome_active');
                 } else { // Если ошибка при авторизации:
                     alert('Неверный логин/пароль');
                 }
+                form.reset(); // Очищаем поля формы
             }
         });
+    });
+
+    // При клике на кнопку выхода:
+    signOutBtn.addEventListener('click', () => {
+        signIn.classList.add('signin_active');
+        welcome.classList.remove('welcome_active');
+        signOutBtn.classList.remove('welcome_active');
+        localStorage.removeItem('user_id');
     });
 });
--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -4,9 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let editor = document.getElementById('editor');
     let clear = document.getElementById('clear');
     // Если в поле была введена информация пользователем ранее, выводим её (из хранилища):
-    if (localStorage.editor !== undefined) {
-        editor.value = localStorage.editor;
-    }
+    editor.value = localStorage.getItem('editor');
     // Записываем введеный текст после нажатия на клавишу в хранилище:
     editor.addEventListener('keyup', () => localStorage.editor = editor.value);
     // При нажатии на кнопку очищаем хранилище:
